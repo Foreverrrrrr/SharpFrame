9f64871246a052b225118db08d1456e9e8b2acab
Forever . <1727365097@qq.com>
2024-03-12 18:57:58 +0800

    添加项目文件。

---
+++ SharpFrame/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using SharpFrame.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using System.Windows;
+
+namespace SharpFrame
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
---
+++ SharpFrame/Views/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace SharpFrame.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SystemLogIn systemLogIn = new SystemLogIn();
+            systemLogIn.Show();
+        }
+    }
+}
